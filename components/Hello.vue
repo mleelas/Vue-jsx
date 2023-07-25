e8e3a21444160cf0cedc36b98e37d0317d1e347e
mleelas <mlsankar@outlook.com>
2023-07-25 11:07:27 +0530

    Initial commit

---
+++ components/Hello.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "hello",
+  data() {
+    return {
+      msg: "Welcome to Vue.js App"
+    };
+  },
+  render() {
+    return (
+      <h1>{ this.msg }</h1>
+    );
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: darkgreen;
+}
+</style>
